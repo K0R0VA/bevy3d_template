707a8970d71d9c9776bb8e7c07abd5e35734ed99
m.yarykov@gmail.com <m.yarykov@gmail.com>
2024-06-28 11:21:14 +0300

    update template; replace 2d with 3d; add app::new

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use bevy::prelude::*;
+use bevy_rapier3d::{plugin::{NoUserData, RapierPhysicsPlugin}, render::{DebugRenderMode, RapierDebugRenderPlugin}};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin {
+            mode: DebugRenderMode::all(),
+            ..Default::default()
+        })
+        .run();
 }
